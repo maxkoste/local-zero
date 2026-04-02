43efcbeb040d9ba95b0c7ce811b8b7631a2da120
Max Koste <60856309+maxkoste@users.noreply.github.com>
2026-04-02 14:42:45 +0200

    added some fancy modern images!

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Hello} from './pages/Frontpage'
+import { Hello } from './pages/Frontpage'
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img className="App-logo" alt="Säkraste sidan på webben!" />
-				<Hello/>
+				<img 
+					className="App-logo" 
+					src="https://i.imgur.com/MK3eW3As.jpg"
+					alt="Säkraste sidan på webben!" 
+				/>
+				<Hello />
 			</header>
 		</div>
 	);
