e497b058c3f9534b3073002e0cda402415a7d4a8
Max Koste <60856309+maxkoste@users.noreply.github.com>
2026-04-09 11:22:39 +0200

    MUI as a dependency - basic navigation implemented

--- src/ui/pages/front-page.tsx
+++ src/ui/pages/front-page.tsx
@@ -6,43 +6,6 @@ import { users } from '../../backend/controller'
 
 //Denna funktionen exporteras och kan importeras genom att skriva:
 //import {Hello} from './Frontpage'
-export function Hello() {
-	return (
-		<div>
-			<h1>Klicka på en av våra användare här nere! </h1>
-			<MyButton />
-		</div>
-	);
-}
-
-function MyButton() {
-	const [selected, setSelected] = useState<User | null>(null);
-
-	let userInfo = null;
-	if (selected != null){
-		userInfo = (
-			<div>
-				<p> ID : {selected.id}</p>
-				<p> Username : {selected.username}</p>
-				<p> Email : {selected.email}</p>
-				<p> Password : {selected.password}</p>
-			</div>
-		)
-	}
-	return (
-		<div>
-			<select onChange={e=>{
-				const user = users.find(u => u.id === Number(e.target.value));
-				setSelected(user ?? null);
-			}}>
-				{ users.map(u => (
-					<option key={u.id} value={u.id}>{u.username}</option>
-				))}
-
-			</select>
-			<div>
-				{userInfo}
-			</div>
-		</div>
-	);
+export function FrontPage() {
+	return <h1>Detta är frontpage</h1>
 }
