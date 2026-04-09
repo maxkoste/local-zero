a6a34a3ff24e8a39db22b77f862612db37df6b33
linnotendal <linn.otendal@gmail.com>
2026-04-09 11:24:35 +0200

    frontpage start

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Hello } from './pages/front-page'
+import FrontPage from "./pages/front-page";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img 
-					className="App-logo" 
-					src="https://i.imgur.com/MK3eW3As.jpg"
-					alt="Säkraste sidan på webben!" 
-				/>
-				<Hello />
-			</header>
-		</div>
-	);
+    return <FrontPage />;
 }
 
 export default App;
